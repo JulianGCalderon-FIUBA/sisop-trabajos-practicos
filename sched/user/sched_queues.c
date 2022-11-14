25abcb4652e3885d4226b9a125fcd6116de3023f
solmoon16 <mmoon@fi.uba.ar>
2022-11-13 23:11:02 -0300

    saco un comentario de primes

--- sched/user/sched_queues.c
+++ sched/user/sched_queues.c
@@ -1,15 +1,4 @@
-// Concurrent version of prime sieve of Eratosthenes.
-// Invented by Doug McIlroy, inventor of Unix pipes.
-// See http://swtch.com/~rsc/thread/.
-// The picture halfway down the page and the text surrounding it
-// explain what's going on here.
-//
-// Since NENVS is 1024, we can print 1022 primes before running out.
-// The remaining two environments are the integer generator at the bottom
-// of main and user/idle.
-
 #include <inc/lib.h>
-
 
 void mark_n(int n);
 
