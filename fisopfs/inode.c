d4fae2804a55ef47275457d1f9d9e6e0e52d521a
Manu <jumdiaz@fi.uba.ar>
2022-11-27 19:42:20 -0300

    added func for inode allocation

--- fisopfs/inode.c
+++ fisopfs/inode.c
@@ -31,8 +31,8 @@ dir_entry_t *read_directory(inode_t *dir, size_t offset) {
 /*
  * 
  */
-int get_inode_by_id(superblock_t *superblock, int inode_id, inode_t **inode_dest) {
-	puts("[debug] get_inode_by_id");
+int get_inode_from_iid(superblock_t *superblock, int inode_id, inode_t **inode_dest) {
+	puts("[debug] get_inode_from_iid");
 	if (inode_id < 0)
 		return EINVAL;
 
@@ -55,7 +55,7 @@ int get_inode_by_id(superblock_t *superblock, int inode_id, inode_t **inode_dest
  * Given a dir and a relative path, returns the inode_id for the file/directory targeted by path
  * If none is found, returns -1.
  */
-int _get_inode_id(superblock_t *superblock, inode_t *dir, char *path) {
+int _get_iid_from_path(superblock_t *superblock, inode_t *dir, char *path) {
 	if (path[0] == '\0') // path was '/' terminated, return the dir
 		return dir->stats.st_ino;
 	char *slash_pos = strchrnul(path, '/');
@@ -76,9 +76,9 @@ int _get_inode_id(superblock_t *superblock, inode_t *dir, char *path) {
 			return dir_entry->inode_id;
 
 		// expecting a dir to continue the search
-		get_inode_by_id(superblock, dir_entry->inode_id, &subdir);
+		get_inode_from_iid(superblock, dir_entry->inode_id, &subdir);
 		if (S_ISDIR(subdir->stats.st_mode)) {
-			return _get_inode_id(superblock, subdir, slash_pos + 1);
+			return _get_iid_from_path(superblock, subdir, slash_pos + 1);
 		}
 		break; // expected a directory (could be a parent dir) but a file was found instead
 	}
@@ -90,12 +90,12 @@ int _get_inode_id(superblock_t *superblock, inode_t *dir, char *path) {
  * Upon error, returns a negated error code
  * path must start with /
  */
-int get_inode_id(superblock_t *superblock, const char *path) {
+int get_iid_from_path(superblock_t *superblock, const char *path) {
 	if (path[0] != '/')
 		return -EINVAL;
 	char path_copy[PATH_MAX];
 	strcpy(path_copy, path);
-	return _get_inode_id(superblock, &superblock->inode_tables[0]->inodes[0], path_copy + 1);
+	return _get_iid_from_path(superblock, &superblock->inode_tables[0]->inodes[0], path_copy + 1);
 }
 
 
@@ -104,6 +104,70 @@ static void init_link(dir_entry_t *dir_entry, int target_inode_id, const char *t
 	dir_entry->inode_id = target_inode_id;
 }
 
+/*
+ * Marks a free inode as occupied, and returns a pointer to the inode.
+ * The inode stats are not initialised, except for the inode_id.
+ * Does not request memory. If there are no free inodes, returns NULL.
+ */
+inode_t *alloc_inode(superblock_t *superblock) {
+	inode_t *inode_dest = NULL;
+	int is_table_free;
+	int free_inode_id;
+	inode_table_t *table;
+	bitmap128_t *free_tables_bmap = &superblock->free_tables_bitmap;
+
+	for (int table_num = 0; table_num < (sizeof(bitmap128_t) * 8); ++table_num) {
+		is_table_free = bitmap_getbit(free_tables_bmap, table_num);
+		if (is_table_free) // table doesn't exist, it has not been allocated
+			continue;
+
+		table = superblock->inode_tables[table_num];
+		bitmap_has_set_bit
+		if (!bitmap_has_set_bit(table->free_inodes_bitmap)) // there are no free inodes in the table
+			continue;
+		free_inode_id = bitmap_most_significant_bit(table->free_inodes_bitmap); // first free inode in table
+		bitmap_clearbit(table->free_inodes_bitmap, free_inode_id); // set inode as occupied
+		inode_dest = &table->inodes[free_inode_id];
+		inode_dest->stats.st_ino = table_num * INODES_PER_TABLE + free_inode_id;
+		break;
+	}
+	return inode_dest;
+}
+
+/*
+ * Marks a free inode as occupied, and returns a pointer to the inode.
+ * Upon failure, returns NULL.
+ * The inode stats are not initialised, except for the inode_id.
+ * Internally requests memory with mmap if necessary, which is later freed by destroy_filesystem
+ */
+inode_t *malloc_inode(superblock_t *superblock) {
+	inode_table_t *table;
+	if ((inode_dest = alloc_inode(superblock_t *superblock)))
+		return inode_dest;
+	
+	// no free inodes, request memory
+	int table_pos = malloc_inode_table(superblock);
+	if (table_pos < 0) {
+		return NULL;
+	}
+	table = superblock->inode_tables[table_pos];
+	bitmap_clearbit(table->free_inodes_bitmap, 0); // set first inode as occupied
+	inode_dest = &table->inodes[0];
+	inode_dest->stats.st_ino = table_num * INODES_PER_TABLE;
+	return inode_dest;
+}
+
+
+
+/*
+ * Returns the position of the table in superblock->inode_tables,
+ * or a negative error code upon failure.
+ */
+int malloc_inode_table(superblock_t *superblock);
+
+/*
+ * Does not set the inode id inside the inode struct, must be done externally
+ */
 int init_dir(inode_t *dir, int inode_id, int parent_inode_id) {
 	// set stats
 	struct stat *dir_st = &dir->stats;
@@ -161,3 +225,8 @@ int init_filesystem(superblock_t *superblock) {
 	root_dir->stats.st_ino = root_inode_id;
 	return EXIT_SUCCESS;
 }
+
+/*
+ * Frees all requested memory.
+ */
+void destroy_filesystem(superblock_t *superblock);
--- fisopfs/inode.h
+++ fisopfs/inode.h
@@ -6,12 +6,12 @@
 #include "bitmap.h"
 
 #define MAX_FILENAME_LENGTH 128
-#define PAGES_PER_INODE 5
+#define PAGES_PER_INODE 5 // amount of pages of memory that a file can point to. Max file size = PAGE_SIZE * PAGES_PER_INODE
 #define PAGE_SIZE 4096 // must be a power of 2
-#define INODES_PER_TABLE (PAGE_SIZE - sizeof(bitmap128_t)) / sizeof(inode_t)
-#define DIR_ENTRIES_PER_PAGE PAGE_SIZE / sizeof(dir_entry_t) // must be at least 3
 #define AMOUNT_OF_INODE_TABLES 128
 
+#define INODES_PER_TABLE (PAGE_SIZE - sizeof(bitmap128_t)) / sizeof(inode_t)
+#define DIR_ENTRIES_PER_PAGE PAGE_SIZE / sizeof(dir_entry_t) // must be at least 3
 
 typedef struct {
 	char name[MAX_FILENAME_LENGTH];
@@ -36,6 +36,14 @@ typedef struct {
 
 
 /*
+ * Marks a free inode as occupied, and returns a pointer to the inode.
+ * Upon failure, returns NULL.
+ * The inode stats are not initialised, except for the inode_id.
+ * Internally requests memory with mmap if necessary, which is later freed by destroy_filesystem
+ */
+inode_t *malloc_inode(superblock_t *superblock);
+
+/*
  * Returns the dir_entry at offset within the dir directory.
  */
 dir_entry_t *read_directory(inode_t *dir, size_t offset);
@@ -44,12 +52,12 @@ dir_entry_t *read_directory(inode_t *dir, size_t offset);
  * Stores in inode_dest a pointer to the inode, or NULL in case of error.
  * The actual inode can be modified through the pointer, it is not a copy.
  */
-int get_inode_by_id(superblock_t *superblock, int inode_id, inode_t **inode_dest);
+int get_inode_from_iid(superblock_t *superblock, int inode_id, inode_t **inode_dest);
 
 /*
  * 
  */
-int get_inode_id(superblock_t *superblock, const char *path);
+int get_iid_from_path(superblock_t *superblock, const char *path);
 
 /*
  * 
