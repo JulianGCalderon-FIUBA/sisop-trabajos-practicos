d9b6012f7b8fbea81113be09e3873db2595ab7d9
Manu <jumdiaz@fi.uba.ar>
2022-11-27 19:41:40 -0300

    added func to test if bitmap evals to true or false

--- fisopfs/bitmap.h
+++ fisopfs/bitmap.h
@@ -2,6 +2,7 @@
 #define BITMAP_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct {
 	uint64_t bitmap64_0;
@@ -35,7 +36,13 @@ void bitmap_clearbit(bitmap128_t *bitmap, int pos);
 
 /*
  * Get the position of the leftmost bit that has a value of 1
+ * Returns -1 if all bits are set to 0
  */
 int bitmap_most_significant_bit(bitmap128_t *bitmap);
+
+/*
+ * Returns true if at least one bit is set to 1
+ */
+bool bitmap_has_set_bit(bitmap128_t *bitmap);
 
 #endif // BITMAP_H
